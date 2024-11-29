f9a1abca52dc6b29e7c49d3c7bacc20a2078cd06
Jeremylaby <barycki@student.agh.edu.pl>
2024-11-29 13:19:37 +0100

    main projektu init

---
+++ droptable/src/main/java/pl/edu/agh/droptable/DroptableApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.droptable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroptableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroptableApplication.class, args);
+	}
+
+}
---
+++ droptable/src/test/java/pl/edu/agh/droptable/DroptableApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.droptable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroptableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
