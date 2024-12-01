c6890ae9e45041f1807f9eeb0ce09517679c7b74
Jeremylaby <barycki@student.agh.edu.pl>
2024-12-01 14:48:06 +0100

    Protected Router Router

--- tabledrop/src/App.jsx
+++ tabledrop/src/App.jsx
@@ -1,21 +1,34 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {AuthProvider} from "./components/AuthProvider.jsx";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import ProtectedRouter from "./components/ProtectedRouter.jsx";
+const roles = {
+    ADMIN: 2,
+    WORKER: 1,
+    USER: 0,
+};
 function App() {
 
 
-  return (
-    <AuthProvider>
-        <Router>
-            <Route path={"/"} element={<Home/>}/>
-            <
-        </Router>
-    </AuthProvider>
-  )
+    return (
+        <AuthProvider>
+            <Router>
+                <Routes>
+                    <Route path={"/"} element={<Home/>}/>
+                    <Route path={"/login"} element={<Login/>}/>
+                    <Route path={"/signup"} element={<SignUp/>}/>
+                    <Route path={"/account"} element={<Account/>}/>
+                    <ProtectedRouter allowedRoles={[0]}>
+                        <Route path={"/admin/users"} element={<UsersList/>}/>
+                    </ProtectedRouter>
+                    <Route path={"/unauthorized"} element={<Unauthorized/>}/>
+                </Routes>
+            </Router>
+        </AuthProvider>
+    )
 }
 
 export default App
---
+++ tabledrop/src/components/ProtectedRouter.jsx
@@ -0,0 +1,18 @@
+import { useAuth} from "./AuthProvider.jsx";
+import {Navigate} from "react-router-dom";
+import PropTypes from "prop-types";
+ProtectedRoute.propTypes = {
+    children: PropTypes.node.isRequired,
+    allowedRoles:  PropTypes.arrayOf(PropTypes.number).isRequired,
+};
+const ProtectedRoute = ({ children, allowedRoles }) => {
+    const { user } = useAuth();
+
+    if (!user || !allowedRoles.includes(user.role)) {
+        return <Navigate to="/unauthorized" />;
+    }
+
+    return children;
+};
+
+export default ProtectedRoute;
