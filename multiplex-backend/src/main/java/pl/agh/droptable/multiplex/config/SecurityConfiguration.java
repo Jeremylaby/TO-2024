1d3b03ddbeae3c185db2a442f0d1e198a0c71d68
Kacper Szot <kacperszot1@gmail.com>
2024-12-02 17:19:03 +0100

    Spring Security Configuration and User Model Definition

---
+++ multiplex-backend/src/main/java/pl/agh/droptable/multiplex/config/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package pl.agh.droptable.multiplex.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
