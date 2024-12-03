f60678e3419b74f1a88170529931149b5425bd96
Gabriela Dumańska <dumanska@student.agh.edu.pl>
2024-12-03 01:58:38 +0100

    Basic login and registration

---
+++ multiplex-backend/src/main/java/pl/agh/droptable/multiplex/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package pl.agh.droptable.multiplex.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.agh.droptable.multiplex.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
