629fba7412ff34407a5eef667ac48dd4925ae29d
Jeremy <barycki@student.agh.edu.pl>
2025-01-19 19:51:14 +0100

    api movie

--- multiplex-frontend/src/pages/MoviePanel.jsx
+++ multiplex-frontend/src/pages/MoviePanel.jsx
@@ -39,7 +39,7 @@ const MovieCreationPanel = () => {
         }
     }
     const addMovie = (movieData) => {
-        return fetch("http://localhost:8080/movie", {
+        return fetch("/api/movie", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
